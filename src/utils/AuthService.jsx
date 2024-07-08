ccb92bb482a881a9f898a4464f699bee8219cbd2
thuannlutitk42 <thuandevk42@gmail.com>
2024-07-08 17:33:43 +0700

    change url product

--- src/utils/AuthService.jsx
+++ src/utils/AuthService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api/auth"
+    baseURL: "https://cybersoftelearningbe-production.up.railway.app/api/auth"
 });
 
 export const signInFunction = async (LoginDto) => {
--- src/utils/QuizService.jsx
+++ src/utils/QuizService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api/quizzes"
+  baseURL: "https://cybersoftelearningbe-production.up.railway.app/api/quizzes"
 })
 
 export const createQuestion = async(quizQustion) =>{
